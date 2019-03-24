e57223819d82841b8134ae5ef613f4b3ec7ea7c9
wouter bolsterlee <wouter@bolsterl.ee>
2019-03-24 21:59:31 +0100

    change grammar to create simpler parse trees

--- sql_json.py
+++ sql_json.py
@@ -10,21 +10,20 @@ grammar = r"""
 
     start: json_path_expr
 
-    json_path_expr: [mode] absolute_path_expr
-    !mode: "strict" | "lax"
+    json_path_expr: [MODE] absolute_path_expr
+    MODE: "strict" | "lax"
 
     absolute_path_expr: "$" path_expr
     relative_path_expr: "@" path_expr
     path_expr: step*
 
-    step: member | element | filter | method
+    ?step: member | element | filter | method
 
     member: "." ("*" | CNAME | ESCAPED_STRING)
 
-    element: "[" (element_wildcard | element_ranges) "]"
-    element_wildcard: "*"
-    element_ranges: element_range ("," element_range)*
-    !element_range: ELEMENT_INDEX ["to" ELEMENT_INDEX]
+    element: "[" ("*" | _element_ranges) "]"
+    _element_ranges: element_range ("," element_range)*
+    element_range: ELEMENT_INDEX ["to" ELEMENT_INDEX]
     ELEMENT_INDEX: INT | "last"
 
     filter: "TODO"
@@ -35,6 +34,9 @@ grammar = r"""
 # todo: signed numbers
 # todo: string quoted members
 # todo: last - number  (ibm)
+# todo: $."$varname" uses the variable name as lookup key
+# todo: json string Unicode escapes
+# todo: match json string semantics (\n and other escapes)
 
 parser = lark.Lark(grammar, parser="lalr", debug=True)
 
